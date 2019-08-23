c08957862ab05bfeafdeb47078384791473c1a99
ytanaka <ytanaka@spec.jp>
2019-08-23 14:51:47 -0700

    パトランプ対応

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let app = express();
 
-//１:モジュールのロード
+//モジュールのロード
 const http = require("http");
 const fs = require("fs");
 
@@ -15,8 +15,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-//４:待ち受け開始
-//server.listen(3000);
+//待ち受け開始
 const server = app.listen(3000, () => {
   console.log("node.js is listning to port:" + server.address().port);
 });
@@ -25,39 +24,39 @@ let returnData = [
     {
       id: "left",
       isOccupied: ""
-    },
-    /*{
-      id: "right",
-      isOccupied: ""
-    }*/
+    }
   ];
 
 // obnizの設定
 const Obniz = require("obniz");
 // センサー　
-const obniz1 = new Obniz("");
+const obniz1 = new Obniz("0667-4000");
 /*
 obniz1を接続後、obniz2の接続
 */
 obniz1.onconnect = async function() {
   // パトランプ
-  const obniz2 = new Obniz("");
+  const obniz2 = new Obniz("3849-2551");
   obniz2.onconnect = async function() {
     const humanDetector = obniz1.wired("HC-SR505", { vcc: 2, gnd: 0, signal: 1 });
-    const ledRed = obniz2.wired("LED", { anode: 7, cathode: 8 });
-    const ledGreen = obniz2.wired("LED", { anode: 9, cathode: 10 });
 
     humanDetector.onchange = function(val) {
       obniz1.display.clear();
       obniz1.display.print(val ? "detected!" : "not detected");
-      if (val) {
-        ledRed.on();
-        ledGreen.off();
+
+      if(val){
+        obniz2.io8.output(false);
+        obniz2.io9.output(true);
+        obniz2.io10.output(false);
+        obniz2.io11.output(true);
       } else {
-        ledRed.off();
-        ledGreen.on();
+        obniz2.io8.output(false);
+        obniz2.io9.output(true);
+        obniz2.io10.output(true);
+        obniz2.io11.output(true);
       }
-      returnData[0].isOccupied = JSON.stringify(val);
+
+      returnData[0].isOccupied = val;
     };
   };
 };
